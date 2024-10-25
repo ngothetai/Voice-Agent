2f534137ea272495667fbbd0b79c2033713242f6
ngothetai <thetai2382002@gmail.com>
2024-10-25 21:52:00 +0700

    add module adaptive-rag

---
+++ adaptive-crag/server.py
@@ -0,0 +1,17 @@
+import functools
+from burr.tracking import LocalTrackingClient
+import importlib
+import os
+from typing import Any, Dict, List, Literal, Optional
+import pydantic
+from fastapi import APIRouter, FastAPI
+from burr.core import Application, ApplicationBuilder
+import application
+
+
+app = FastAPI()
+router = APIRouter()
+
+class ConversationBOTVOV(pydantic.BaseModel):
+    message: str
+    
